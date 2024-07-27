33765ba5a6752a40fab517437b47e8fad791a76f
indu <70087467+lazyindu@users.noreply.github.com>
2024-07-27 22:48:47 +0530

    Add files via upload

--- lazybot/clients.py
+++ lazybot/clients.py
@@ -1,16 +1,21 @@
+    # Credit @LazyDeveloper.
+    # Please Don't remove credit.
+    # Born to make history @LazyDeveloper !
+
+    # Thank you LazyDeveloper for helping us in this Journey
+    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+
+    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
 import asyncio
 import logging
-from pyrogram import Client
 from info import *
-from . import multi_clients, work_loads, LazyPrincessXBot
+from pyrogram import Client
+from util.config_parser import TokenParser
+from . import multi_clients, work_loads, LazyPrincessBot
 
-class TokenParser:
-    def parse_from_env(self):
-        tokens = BOT_TOKEN
-        return {i: token for i, token in enumerate(tokens)}
 
 async def initialize_clients():
-    multi_clients[0] = LazyPrincessXBot("default_bot", BOT_TOKEN[0])
+    multi_clients[0] = LazyPrincessBot
     work_loads[0] = 0
     all_tokens = TokenParser().parse_from_env()
     if not all_tokens:
@@ -20,18 +25,27 @@ async def initialize_clients():
     async def start_client(client_id, token):
         try:
             print(f"Starting - Client {client_id}")
-            client = LazyPrincessXBot(name=f"bot_{client_id}", bot_token=token)
-            await client.start()
+            if client_id == len(all_tokens):
+                await asyncio.sleep(2)
+                print("This will take some time, please wait...")
+            client = await Client(
+                name=str(client_id),
+                api_id=API_ID,
+                api_hash=API_HASH,
+                bot_token=token,
+                sleep_threshold=SLEEP_THRESHOLD,
+                no_updates=True,
+                in_memory=True
+            ).start()
             work_loads[client_id] = 0
             return client_id, client
-        except Exception as e:
-            logging.error(f"Failed starting Client - {client_id} Error: {e}", exc_info=True)
+        except Exception:
+            logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
     
     clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
-    clients = [client for client in clients if client is not None]  # Filter out failed clients
     multi_clients.update(dict(clients))
-    
-    if len(multi_clients) > 1:
+    if len(multi_clients) != 1:
+        MULTI_CLIENT = True
         print("Multi-Client Mode Enabled")
     else:
         print("No additional clients were initialized, using default client")
